c4b7fc4d1e70a76924164df3d97a7700db98c9ac
JYL27 <junyulee27@gmail.com>
2024-01-23 09:20:03 +0800

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,60 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+Post.create!([{
+    id: 1,
+    poster: "jy",
+    title: "Hello World",
+    body: "Goodbye World",
+    tag: "Technology"
+}, 
+{
+    id: 2,
+    poster: "jx",
+    title: "This is post number 2",
+    body: "This is body number 2",
+    tag: "General"
+}])
+
+Comment.create!([{
+    id: 1,
+    commenter: "jx",
+    body: "Here's a sample comment",
+    post_id: 1,
+    parent_id: null
+}, 
+{   
+    id: 2,
+    commenter: "jy",
+    body: "Here's a sample reply",
+    post_id: 1,
+    parent_id: 1
+},
+{   
+    id: 3,
+    commenter: "jx",
+    body: "Here's a reply to a reply",
+    post_id: 1,
+    parent_id: 2
+}, 
+{   
+    id: 4,
+    commenter: "jz",
+    body: "Here's another sample reply",
+    post_id: 1,
+    parent_id: 1
+},
+{   
+    id: 5,
+    commenter: "jy",
+    body: "Here's another sample comment",
+    post_id: 1,
+    parent_id: null
+}, 
+{   
+    id: 6,
+    commenter: "jz",
+    body: "Here's another sample reply",
+    post_id: 1,
+    parent_id: 5
+}])
